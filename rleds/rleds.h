700b482890eee92beee7e6b37ed30b4c209a7801
Pieter Hollants <pieter@hollants.com>
2006-09-30 14:52:24 +0000

    Larger rewrite in anticipation of extended functionality: - LED <-> pin wiring is no longer hardcoded but specified on cmdline - support for dual-color LEDs by driving an optional second pin - second/third color will be used for special support of   - Ethernet   - WLAN   - PPP   - VPN devices
    LEDs are no longer allocated in a continous memory block. Instead managed
    in a single-linked list.
    
    We use getopt for command line argument parsing, including support for
    long options.
    
    More useful error messages and a new command line arguments help text.
    
    Replaced stupid pow() replacement multiplication with simple and fast bit
    shifting to get required values for writing to parallel port data register.
    
    
    git-svn-id: file:///home/pief/svn/rleds/trunk@10 822f2e9a-261e-0410-a646-d699a81b6f75

--- rleds/rleds.h
+++ rleds/rleds.h
@@ -11,20 +11,51 @@
 ** or FITNESS FOR A PARTICULAR PURPOSE.
 */
 
-/* Default parport device to use */
-#define DEFAULT_PPDEV "/dev/parport0"
+/* Default LED device to use */
+#define DEFAULT_LEDDEV "/dev/parport0"
+
+/* Internal value used to signalize request for help with command line options */
+#define OPT_HELP 1
+
+/* The different kinds of interfaces supported (only relevant for dual-color LEDs) */
+typedef enum _iftype
+{
+	IFTYPE_GENERIC,				/* Generic device (no special LED state support) */
+	IFTYPE_ETHERNET,			/* Ethernet device (2nd pin used in signaling link speed) */
+	IFTYPE_WLAN,				/* WLAN device (2nd pin used in signaling associations) */
+	IFTYPE_PPP,				/* PPP device (2nd pin used in signaling connection estd.) */
+	IFTYPE_VPN				/* VPN device (2nd pin used in signaling VPN state) */
+} IFTYPE;
+
+/* LED color states */
+typedef enum _ledcolor
+{
+	LEDCOLOR_PRIM,				/* Single color LED or primary LED pin enabled */
+	LEDCOLOR_SEC,				/* Secondary LED pin enabled */
+	LEDCOLOR_BOTH				/* Both LED pins enabled */
+} LEDCOLOR;
 
 /* Mangement structure to keep track of LEDs and the associated interface */
-typedef struct _led
+typedef struct _led LED;
+struct _led
 {
-	char		*if_path,		/* Path of sysfile dir for interface */
+	LED		*next;			/* Next LED in linked list */
+
+	char		*if_name;		/* Interface belonging to this LED */
+	IFTYPE		if_type;		/* Type of interface */
+
+	char		prim_pin,		/* Number of primary LED pin */
+			sec_pin;		/* Number of secondary LED pin (for dual-color LED, may be 0) */
+	LEDCOLOR	color;			/* Current color of LED */
+	char		on;			/* LED enabled (used for blinking)? */
+
+	char		up,			/* Interface up? (= sysfs paths present?) */
+			*if_path,		/* Path of sysfs dir for interface */
 			*rx_path,		/* Path of sysfs file for rx_packets value */
 			*tx_path;		/* Path of sysfs file for tx_packets value */
 	long int 	rx_packets,		/* Last remembered rx_packets value */
 			tx_packets;		/* Last remembered tx_packets value */
-	char		up,			/* Interface up? (= sysfs paths present?) */
-			on;			/* LED on? */
-} LED;
+};
 
 /* sysfs prefix to interface data (with trailing slash) */
 #define SYSFS_PREFIX "/sys/class/net/"
